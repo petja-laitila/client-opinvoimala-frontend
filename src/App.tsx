d9ad4de2dad9168f9d46b575e801ffac878595ca
WeceW <toni.weckroth@gmail.com>
2021-09-20 16:05:49 +0300

    Updated husky config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React, { FC } from 'react';
 
 const App: FC = () => {
-  return <div className="App">Opinvoimala
-  
-
-  
-
-  </div>;
+  return <div className="App">Opinvoimala</div>;
 };
 
 export default App;
