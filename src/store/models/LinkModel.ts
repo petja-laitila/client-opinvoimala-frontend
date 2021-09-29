47ca9e25a31355288f57918badae3e348b282c20
WeceW <toni.weckroth@gmail.com>
2021-09-29 10:36:03 +0300

    Add target page attribute to link model

--- src/store/models/LinkModel.ts
+++ src/store/models/LinkModel.ts
@@ -3,6 +3,7 @@ import { SnapshotOut, types } from 'mobx-state-tree';
 export const LinkModel = types.model({
   label: types.maybeNull(types.string),
   url: types.maybeNull(types.string),
+  target_page: types.maybeNull(types.number),
 });
 
 export interface Link extends SnapshotOut<typeof LinkModel> {}
