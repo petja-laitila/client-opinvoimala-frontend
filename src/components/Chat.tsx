b260446478abdb85a1fce5c9edb8115eeb2f8b92
WeceW <toni.weckroth@gmail.com>
2023-03-18 12:40:09 +0200

    OPI-114: Changed giosg (chat) script to new version

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -13,9 +13,9 @@ const Chat: React.FC<Props> = ({ giosgCompanyId }) => {
       <script>
         {`
           (function(w, t, f) {
-            var s='script',o='_giosg',h='https://service.giosg.com',e,n;e=t.createElement(s);e.async=1;e.src=h+'/live/';
-            w[o]=w[o]||function(){(w[o]._e=w[o]._e||[]).push(arguments)};w[o]._c=f;w[o]._h=h;n=t.getElementsByTagName(s)[0];n.parentNode.insertBefore(e,n);
-          })(window,document,${giosgCompanyId});
+            var s='script',o='_giosg',h='https://service.giosg.com',e,n;e=t.createElement(s);e.async=1;e.src=h+'/live2/'+f;
+            w[o]=w[o]||function(){(w[o]._e = w[o]._e || []).push(arguments)};w[o]._c=f;w[o]._h=h;n=t.getElementsByTagName(s)[0];n.parentNode.insertBefore(e,n);
+          })(window,document,"${giosgCompanyId}");
         `}
       </script>
     </Helmet>
